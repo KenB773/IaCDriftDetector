f86987a2b2e9f9c1816c94accda654339ecff71a
Ken B <160813236+KenB773@users.noreply.github.com>
2025-04-19 19:40:31 -0400

    Update awsfetcher.go

--- internal/awsfetcher.go
+++ internal/awsfetcher.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
+	//Go breaks (unused import) if this is left in during a dry run "github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
