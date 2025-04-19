49d586b4b63fd3b0b913ea9beecc01d0049221d1
Ken B <160813236+KenB773@users.noreply.github.com>
2025-04-19 18:59:19 -0400

    Update tfstateparser.go
    Added full functionality

--- internal/tfstateparser.go
+++ internal/tfstateparser.go
@@ -1,4 +1,43 @@
 // Terraform state parser
 package internal
 
-// TODO: Parse Terraform state files
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type TFState struct {
+	Resources []TFResource `json:"resources"`
+}
+
+type TFResource struct {
+	Type      string               `json:"type"`
+	Name      string               `json:"name"`
+	Instances []TFResourceInstance `json:"instances"`
+}
+
+type TFResourceInstance struct {
+	Attributes map[string]interface{} `json:"attributes"`
+}
+
+// ParseTFState loads and parses a Terraform .tfstate file from disk.
+func ParseTFState(filePath string) (*TFState, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var state TFState
+	if err := json.Unmarshal(bytes, &state); err != nil {
+		return nil, err
+	}
+
+	return &state, nil
+}
