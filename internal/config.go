831739146027bee7499b35c6a60c197cbda4510f
Ken B <160813236+KenB773@users.noreply.github.com>
2025-04-19 19:09:59 -0400

    Update config.go
    YAML config file functionality added

--- internal/config.go
+++ internal/config.go
@@ -1,4 +1,31 @@
 // Config file parser
 package internal
 
-// TODO: Parse optional YAML config files
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Config struct {
+	Region      string   `yaml:"region"`
+	Include     []string `yaml:"include"`
+	Output      string   `yaml:"output"`
+	OutputFile  string   `yaml:"output_file"`
+	StateFile   string   `yaml:"state_file"`
+	DryRun      bool     `yaml:"dry_run"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(file, &cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
